a663bb71a7ef54ae37444cb893e36ac38ce10ed3
Maximiliano Mendez <maximiliano.mendez@opt-media.net>
2021-04-14 10:58:59 +0200

    Creados los test correctamente para los components de Registro y login, faltará hacer el test del service y algunos arreglos finales para la entrega de la prueba técnica

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -1,32 +1,76 @@
-
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-import { RegisterComponent } from './register.component';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { async, ComponentFixture, fakeAsync, TestBed } from "@angular/core/testing";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
+import { RouterTestingModule } from "@angular/router/testing";
+import { RegisterComponent } from "./register.component";
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+  let component:RegisterComponent;
+  let fixture:ComponentFixture<RegisterComponent>;
+  let el: HTMLElement;
+  const formBuilder:FormBuilder = new FormBuilder();
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(async()=>{
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ],
-      imports:[         
-        FormsModule, 
-        RouterTestingModule.withRoutes([]),
-        ReactiveFormsModule],
-    
-    })
-    .compileComponents();
-  }));
+      declarations:[RegisterComponent],
+      imports:[ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule.withRoutes([])],
+      providers:[RouterTestingModule ]
+    }).compileComponents();
+  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RegisterComponent);
+  beforeEach(()=>{
+    fixture=TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should set submitted to true', async(() => {
+    component.registerUser();
+    expect(component.registerUser).toBeTruthy();
+  }));
+
+ it('Should call the Register User method', () =>{ fakeAsync(() =>{
+   fixture.detectChanges();
+   spyOn(component,'registerUser');
+   el=fixture.debugElement.query(By.css('Register')).nativeElement;
+   el.click();
+   expect(component.registerUser).toHaveBeenCalledTimes(0);
+ })
+
+ });
+
+  it('Form should be invalid', async(()=> {
+    component.registerForm.controls['first_name'].setValue('');
+    component.registerForm.controls['last_name'].setValue('');
+    component.registerForm.controls['username'].setValue('');
+    component.registerForm.controls['email'].setValue('');
+    component.registerForm.controls['password'].setValue('');
+    expect(component.registerForm.valid).toBeFalsy();
+  }));
+
+  it('Form should be valid', async(()=> {
+    component.registerForm.controls['first_name'].setValue('Maxi');
+    component.registerForm.controls['last_name'].setValue('Mendez');
+    component.registerForm.controls['username'].setValue('maxi');
+    component.registerForm.controls['email'].setValue('maximendez@suweb.com.uy');
+    component.registerForm.controls['password'].setValue('Pruebas');
+    component.registerForm.controls['passwordConfirm'].setValue('Pruebas');
+    expect(component.registerForm.valid).toBeTruthy();
+  }));
+
+  it('Form password validation, the passwords must to be equals', async(()=>{
+    let pass=component.registerForm.controls['password'].setValue('Prueba')
+    let passConf=component.registerForm.controls['passwordConfirm'].setValue('Prueba')
+    if(pass===passConf){
+      expect(component.registerForm).toBeTruthy();
+    }else{
+      expect(component.registerForm).toBeFalsy();
+    }
+  }));
+
 });
