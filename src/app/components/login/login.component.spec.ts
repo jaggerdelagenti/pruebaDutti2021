9938aaa6ed099c7183433728c2f0f8f1de4ab553
Maximiliano Mendez <maximendez@suweb.com.uy>
2021-04-14 12:33:16 +0200

    Creados los tests para los componentes de Register y Login, quedarán los últimos retoques para la entrega con todos los requerimientos de la prueba técnica

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,19 +1,21 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { async, ComponentFixture, fakeAsync, TestBed } from "@angular/core/testing";
 import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
 import { RouterTestingModule } from "@angular/router/testing";
 import { LoginComponent } from "./login.component";
 
-fdescribe('LoginComponent', () => {
+describe('LoginComponent', () => {
   let component:LoginComponent;
   let fixture:ComponentFixture<LoginComponent>;
-  
+  let el: HTMLElement;
   const formBuilder:FormBuilder = new FormBuilder();
 
   beforeEach(async()=>{
     TestBed.configureTestingModule({
       declarations:[LoginComponent],
-      imports:[ReactiveFormsModule],
-      providers:[RouterTestingModule]
+      imports:[ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule.withRoutes([])],
+      providers:[RouterTestingModule ]
     }).compileComponents();
   });
 
@@ -23,6 +25,35 @@ fdescribe('LoginComponent', () => {
     fixture.detectChanges();
   })
 
-  
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Should set submitted to true', async(() => {
+    component.loginUser();
+    expect(component.loginUser).toBeTruthy();
+  }));
+
+ it('Should call the Login User method', () =>{ fakeAsync(() =>{
+   fixture.detectChanges();
+   spyOn(component,'loginUser');
+   el=fixture.debugElement.query(By.css('Login')).nativeElement;
+   el.click();
+   expect(component.loginUser).toHaveBeenCalledTimes(0);
+ })
+
+ });
+
+  it('Form should be invalid', async(()=> {
+    component.loginForm.controls['username'].setValue('');
+    component.loginForm.controls['password'].setValue('');
+    expect(component.loginForm.valid).toBeFalsy();
+  }));
+
+  it('Form should be valid', async(()=> {
+    component.loginForm.controls['username'].setValue('admin');
+    component.loginForm.controls['password'].setValue('admin123');
+    expect(component.loginForm.valid).toBeTruthy();
+  }));
 
 });
--- src/app/services/users.service.spec.ts
+++ src/app/services/users.service.spec.ts
@@ -1,16 +1,29 @@
-// import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { inject, TestBed } from "@angular/core/testing";
+import { UserService } from "./users.service";
 
-// import { UserService } from './users.service';
+const fakeUsername="admin";
+const fakePass="Pruebas123";
 
-// describe('UsersService', () => {
-//   let service: UserService;
-
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(UserService);
-//   });
-
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+describe('Service: List Service', () => {
+    let httpMock: HttpTestingController;
+    let service: UserService;
+  
+    beforeEach(() => {
+      TestBed.configureTestingModule({ imports: [HttpClientTestingModule], providers: [UserService] });
+    });
+  
+    beforeEach(inject([UserService, HttpTestingController], (_service, _httpMock) => {
+      service = _service;
+      httpMock = _httpMock;
+    }));
+  
+    it('login must return ok', () => {
+      service.login(fakeUsername,fakePass).then(user => {
+        expect(fakeUsername).toBeTruthy();
+        expect(fakePass).toBeTruthy();
+    });
+  
+      
+    });
+  });
